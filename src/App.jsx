3be71245234d86807d55446e6a0edf91f03f3b53
YanDevv - Macbook <ybritogomes@gmail.com>
2026-01-18 10:25:07 -0300

    feat(converter): adicionando currency freals API para converter moedas diversas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from "react"
+import { createContext, useEffect, useState } from "react";
+import { useContext } from "react";
 
 function App() {
-  const [valorReais, setValorReais] = useState('');
-  const [valorDolares, setValorDolares] = useState('');
+  const apiKey = 'YOUR_API_KEY_HERE'; // Substitua pela sua chave de API
+
+  const [exchanges, setExchanges] = useState(null);
+  const [currencies, setCurrencies] = useState(null);
+
+  const [deMoeda, setDeMoeda] = useState('BRL');
+  const [paraMoeda, setParaMoeda] = useState('USD');
+  const [deValor, setDeValor] = useState('');
+  const [paraValor, setParaValor] = useState('');
+
+  const MainContext = createContext({
+    exchanges,
+    setExchanges,
+    currencies,
+    setCurrencies,
+  });
+  const useMainContext = useContext(MainContext);
 
   function converterMoeda() {
-    const valorEmReais = parseFloat(valorReais.replace(',', '.'));
-    if (!isNaN(valorEmReais)) {
-      const valorConvertido = valorEmReais / 5.4;
-      setValorDolares(valorConvertido.toFixed(2).replace('.', ','));
+    if (!exchanges) return; // TODO: tratar erro
+
+    const cotacaoDeMoeda = exchanges[deMoeda];
+    const cotacaoParaMoeda = exchanges[paraMoeda];
+
+    if (deMoeda === 'USD') {
+      setParaValor((deValor * cotacaoParaMoeda).toFixed(2));
+    } else if (paraMoeda === 'USD') {
+      setParaValor((deValor / cotacaoDeMoeda).toFixed(2));
     } else {
-      setValorDolares('');
+      const valorEmDolar = deValor / cotacaoDeMoeda;
+      setParaValor((valorEmDolar * cotacaoParaMoeda).toFixed(2));
     }
   }
 
@@ -18,35 +40,88 @@ function App() {
     converterMoeda();
   }
 
-  function handleInputReais(event) {
-    setValorReais(event.target.value);
+  function handleInputDeMoeda(event) {
+    setDeMoeda(event.target.value);
   }
+
+  function handleInputParaMoeda(event) {
+    setParaMoeda(event.target.value);
+  }
+
+  function handleInputDeValor(event) {
+    setDeValor(event.target.value);
+  }
+
+  useEffect(() => {
+    if (!useMainContext.exchanges) {
+      const fetchExchangeRates = async () => {
+        const response = await fetch(`https://api.currencyfreaks.com/v2.0/rates/latest?apikey=${apiKey}`);
+
+      const data = await response.json();
+
+      useMainContext.setExchanges(data.rates);
+
+      const currenciesArray = Object.keys(data.rates);
+      currenciesArray.sort((currencyA, currencyB) => {
+        return currencyA.localeCompare(currencyB);
+      });
+
+      useMainContext.setCurrencies(currenciesArray);
+    };
+
+      fetchExchangeRates();
+    }
+  }, []);
 
   return (
     <>
       <div className="w-full flex flex-col items-center h-screen justify-center">
         <h1 className="text-center pt-8">Conversor de Moedas</h1>
-        <h2 className="text-center pt-2">De Reais para Dólares</h2>
-        <div className="pt-4">
-          <label htmlFor="valor" className="block text-left">Valor em Reais:</label>
+        <div className="flex gap-4 items-center pt-4">
+          <label htmlFor="inputDeValor" className="block text-left font-semibold">De:</label>
           <input
             type="text"
-            id="valor"
-            placeholder="Digite o valor em Reais"
-            className="border border-gray-300 p-2 rounded-md w-80 mt-4"
-            onChange={handleInputReais}
+            id="inputDeValor"
+            // placeholder=""
+            className="border border-gray-300 p-2 rounded-md w-80"
+            onChange={handleInputDeValor}
           />
+          <select
+            className={`border border-gray-300 p-2 rounded-md w-fit`}
+            disabled={!exchanges}
+            onChange={handleInputDeMoeda}
+            value={deMoeda}
+          >
+            {useMainContext.currencies ? useMainContext.currencies.map((moeda) => (
+              <option key={moeda} value={moeda}>{moeda}</option>
+            )) : <option>Carregando...</option>}
+          </select>
+        </div>
+        <div className="pt-4 flex gap-4 items-center">
+          <label htmlFor="inputParaValor" className="block text-left font-semibold">Para:</label>
+          <input
+            type="text"
+            id="inputParaValor"
+            disabled={exchanges}
+            // placeholder=""
+            className="border border-gray-300 p-2 rounded-md w-80"
+            value={paraValor}
+          />
+          <select
+            className={`border border-gray-300 p-2 rounded-md w-fit`}
+            disabled={!exchanges}
+            onChange={handleInputParaMoeda}
+            value={paraMoeda}
+          >
+            {useMainContext.currencies ? useMainContext.currencies.map((moeda) => (
+              <option key={moeda} value={moeda}>{moeda}</option>
+            )) : <option>Carregando...</option>}
+          </select>
         </div>
         <button
           className="mt-6 bg-blue-500 text-white px-4 py-2 rounded-md"
           onClick={handleConvert}
         >Converter</button>
-        {valorDolares && (
-          <div>
-            <h2 className="mt-6">Valor Convertido:</h2>
-            <p className="text-2xl font-bold mt-2">U$ {valorDolares}</p>
-          </div>
-        )}
       </div>
     </>
   )
